a19213e59859811faf319c108486145953819bf3
Abraham Adugna <abrahamadugnaurgessa2@gmail.com>
2025-05-21 22:56:02 +0300

    Delete how-to.js

--- how-to.js
+++
@@ -1,7 +0,0 @@
-// methods are defined on an object or variable 
-// functions are defined by their own
-let name = "abraham";
-
-// calling method
-let result1 = name.toUpperCase();
-console.log(result1);
