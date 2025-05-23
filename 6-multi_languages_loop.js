52dd486f2080644859835b6ca1d9fdca05d4f12c
Abraham Adugna <abrahamadugnaurgessa2@gmail.com>
2025-05-23 16:51:09 +0300

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,11 +1,4 @@
-let langs = ["C is fun" , "Python is cool" , "JavaScript is amazing"];
-let i=0;
-while(i < langs.length){
-    console.log(langs[i]);
-    i++
-}
-
-/*for(let i= 0 ; i < langs.length ; i++){
+for(let i=0; i < langs.length; i++){
     let langs = ["C is fun" , "Python is cool" , "JavaScript is amazing"];
     console.log(langs[i]);
-} */
+} 
